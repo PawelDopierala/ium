d19f741dbf9d4d76df5ff15ef97443d345076687
PawelDopierala <pawel.dopierala4@gmail.com>
2024-04-02 23:18:46 +0200

    try

--- data_processing.py
+++ data_processing.py
@@ -1,10 +1,11 @@
 import sklearn
+from sklearn.model_selection import train_test_split
 import pandas as pd
 
 housing_price_dataset = pd.read_csv('housing_price_dataset.csv')
 
-hp_train_test, hp_dev = sklearn.model_selection.train_test_split(housing_price_dataset, test_size=0.1)
-hp_train, hp_test = sklearn.model_selection.train_test_split(hp_train_test, test_size=1000)
+hp_train_test, hp_dev = train_test_split(housing_price_dataset, test_size=0.1)
+hp_train, hp_test = train_test_split(hp_train_test, test_size=1000)
 
 hp_train = pd.get_dummies(hp_train, columns=['Neighborhood'])
 hp_dev = pd.get_dummies(hp_dev, columns=['Neighborhood'])
