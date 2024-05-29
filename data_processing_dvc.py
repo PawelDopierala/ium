b586cbdbc494045c56555f7ac7c51bb9fb6b4bd3
PawelDopierala <pawel.dopierala4@gmail.com>
2024-05-29 21:22:02 +0200

    add pipeline dvc

---
+++ data_processing_dvc.py
@@ -0,0 +1,14 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import sys
+
+housing_price_dataset = pd.read_csv(sys.argv[1])
+
+housing_price_dataset = pd.get_dummies(housing_price_dataset, columns=['Neighborhood'])
+
+hp_train_test, hp_dev = train_test_split(housing_price_dataset, test_size=0.1)
+hp_train, hp_test = train_test_split(hp_train_test, test_size=1000)
+
+hp_train.to_csv('hp_train.csv', index=False)
+hp_dev.to_csv('hp_dev.csv', index=False)
+hp_test.to_csv('hp_test.csv', index=False)
